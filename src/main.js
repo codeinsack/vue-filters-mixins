3bb5b12e2f53f6706b4fdb690b64f8aeb03af59a
codeinsack <codeinsack@gmail.com>
2020-02-03 20:38:39 +0300

    global filter

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 
 import App from './App.vue'
 
+Vue.filter('toLowercase', function (value) {
+  return value.toLowerCase()
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
